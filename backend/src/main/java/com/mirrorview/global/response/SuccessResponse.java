d4f8f09d1822e1b0d6f507b3a3e0096d7593fcea
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2023-07-19 22:41:26 +0900

    feat: make BaseResponse

---
+++ backend/src/main/java/com/mirrorview/global/response/SuccessResponse.java
@@ -0,0 +1,16 @@
+package com.mirrorview.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SuccessResponse {
+	private boolean success;
+	private String msg;
+	private Object data;
+}
