487d69e3a3e8772c82df2f3db5cd6383606c566e
정원식 <ws_45@naver.com>
2023-07-25 15:57:17 +0900

    feat : home화면 디자인 및 about화면 디자인

---
+++ frontend/src/pages/Aboutus/bubble.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SearchBar = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  background-color: #ffffff;
+  border: 0px solid #ccc;
+  border-radius: 30px;
+  padding: 65px 55px;
+  width: 330%;
+  max-width: 2000px;
+
+  &::before {
+    content: '';
+    position: absolute;
+    transform: translateY(-50%);
+    width: 20;
+    height: 0;
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-bottom: 20px solid #ffffff; /* 꼬리 색상을 배경색과 동일하게 설정 */
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 20%;
+    left: -38px; /* 꼬리 뾰족한 부분을 왼쪽 옆을 향하도록 설정 */
+    transform: translateY(-50%);
+    width: 10;
+    height: 10;
+    border-left: 20px solid transparent;
+    border-right: 20px solid #ffffff; /* 꼬리 뾰족한 부분의 색상 설정 */
+    border-bottom: 20px solid transparent;
+  }
+`;
+
+const Bubble = () => {
+  return (
+    <SearchBar>
+      {/* 꼬리 모양이 없으므로 BubbleTail 컴포넌트를 제거합니다. */}
+    </SearchBar>
+  );
+};
+
+export default Bubble;
---
+++ frontend/src/pages/Aboutus/bubble6.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SearchBar = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  background-color: #fef01b;
+  border: 0px solid #ccc;
+  border-radius: 30px;
+  padding: 35px 30px;
+  width: 320%;
+  max-width: 2000px;
+
+
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 43%;
+    left: 360px; /* 꼬리 뾰족한 부분을 왼쪽 옆을 향하도록 설정 */
+    transform: translateY(-50%) rotate(270deg);
+    width: 10;
+    height: 10;
+    border-left: 20px solid transparent;
+    border-right: 20px solid #fef01b; /* 꼬리 뾰족한 부분의 색상 설정 */
+    border-bottom: 20px solid transparent;
+  }
+`;
+
+const Bubble6= () => {
+  return (
+    <SearchBar>
+      {/* 꼬리 모양이 없으므로 BubbleTail 컴포넌트를 제거합니다. */}
+    </SearchBar>
+  );
+};
+
+export default Bubble6;
