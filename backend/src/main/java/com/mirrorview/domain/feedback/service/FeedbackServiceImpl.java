540a47f8caef6db41e117873a0ce0a864da66e7d
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2023-07-30 21:48:32 +0900

    feat: 피드백 저장 기능 추가

--- backend/src/main/java/com/mirrorview/domain/feedback/service/FeedbackServiceImpl.java
+++ backend/src/main/java/com/mirrorview/domain/feedback/service/FeedbackServiceImpl.java
@@ -1,14 +1,20 @@
 package com.mirrorview.domain.feedback.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.mirrorview.domain.essay.domain.EssayDetail;
+import com.mirrorview.domain.essay.repository.EssayDetailRepository;
 import com.mirrorview.domain.feedback.domain.Feedback;
 import com.mirrorview.domain.feedback.dto.FeedbackDto;
+import com.mirrorview.domain.feedback.dto.FeedbackSaveDto;
 import com.mirrorview.domain.feedback.repository.FeedbackRepository;
+import com.mirrorview.domain.user.domain.Member;
+import com.mirrorview.domain.user.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +23,8 @@ import lombok.RequiredArgsConstructor;
 public class FeedbackServiceImpl implements FeedbackService {
 
 	private final FeedbackRepository feedbackRepository;
+	private final EssayDetailRepository essayDetailRepository;
+	private final MemberRepository memberRepository;
 
 	@Override
 	public List<FeedbackDto> findFeedbackByUserId(String userId) {
@@ -24,7 +32,7 @@ public class FeedbackServiceImpl implements FeedbackService {
 	}
 
 	@Override
-	public List<FeedbackDto> findFeedbackByRoomId(Long roomId){
+	public List<FeedbackDto> findFeedbackByRoomId(Long roomId) {
 		return feedbackRepository.findFeedbackByRoomId(roomId);
 	}
 
@@ -38,4 +46,20 @@ public class FeedbackServiceImpl implements FeedbackService {
 	public void deleteFeedbackByFeedbackId(Long feedbackId) {
 		feedbackRepository.deleteFeedbackByFeedbackId(feedbackId);
 	}
+
+	@Override
+	public void saveFeedback(FeedbackSaveDto dto) {
+		Optional<EssayDetail> essayDetail = essayDetailRepository.findById(dto.getEssayDetailId());
+		Optional<Member> member = memberRepository.findById(dto.getUserId());
+		if (essayDetail.isPresent() && member.isPresent()) {
+			Feedback feedback = Feedback.builder()
+				.content(dto.getContent())
+				.roomId(dto.getRoomId())
+				.essayDetail(essayDetail.get())
+				.writer(member.get())
+				.build();
+			feedbackRepository.save(feedback);
+		}
+	}
+
 }
