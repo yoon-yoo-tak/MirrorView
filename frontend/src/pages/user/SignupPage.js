8aca2fe5a1914551a818c6a1a58abcd5c3fb3a4f
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2023-08-08 00:05:30 +0900

    feat: 회원가입 api 연결

--- frontend/src/pages/user/SignupPage.js
+++ frontend/src/pages/user/SignupPage.js
@@ -1,5 +1,5 @@
 import * as S from "../../components/mypage/MypageStyledComponents";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {
@@ -35,15 +35,20 @@ const Signup = () => {
         email: "",
         nickname: "",
         username: "",
-        fullName: "",
     });
 
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [passwordMatchError, setPasswordMatchError] = useState(false); // 새로운 상태 추가
+    const [passwordMatchError, setPasswordMatchError] = useState(null); // 새로운 상태 추가
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const [inputCheckPassword, setInputCheckPassword] = useState("");
+    const [inputCheckPassword, setInputCheckPassword] = useState(null);
+    const [userIdCheck, setUserIdCheck] = useState(null);
+    const [userNickNameCheck, setUserNickNameCheck] = useState(null);
+    const [userEmailCheck, setEmailCheck] = useState(null);
+    const [verificationCode, setVerificationCode] = useState(null);
+    const [verificationCodeValid, setVerificationCodeValid] = useState(null);
+    const [emailValid,setEmailValid] = useState(false);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -51,23 +56,30 @@ const Signup = () => {
             ...prevData,
             [name]: value,
         }));
-        setPasswordMatchError(e.target.value !== confirmPassword);
+        // setPasswordMatchError(e.target.value !== confirmPassword);
     };
+
+    useEffect(() => {
+        console.log(formData);
+    }, [formData])
+
+
     const onClickCheckId = (e) => {
         e.preventDefault();
         if (formData.userId === "") {
             alert("아이디를 입력해주세요.");
         } else {
             axios({
-                url: `http://localhost:8000/api/users/${formData.userId}/check-id`,
+                url: `/api/users/${formData.userId}/check-id`,
                 method: "GET",
             })
                 .then((res) => {
                     alert(res.data.msg);
+                    setUserIdCheck(true);
                 })
                 .catch((err) => {
                     alert("이미 잇는 아이디");
-                    console.log("에러임");
+                    setUserIdCheck(false);
                 });
         }
     };
@@ -77,26 +89,52 @@ const Signup = () => {
             alert("닉네임을 입력해 주세요.");
         } else {
             axios({
-                url: `http://localhost:8000/api/users/${formData.nickname}/check-nickname`,
+                url: `/api/users/${formData.nickname}/check-nickname`,
                 method: "GET",
             })
                 .then((res) => {
                     alert(res.data.msg);
+                    setUserNickNameCheck(true);
                 })
                 .catch((err) => {
                     alert("이미 잇는 닉네임");
+                    setUserNickNameCheck(false);
                 });
         }
     };
 
+    const handleEmail = (e) => {
+        const value = e.target.value;
+        const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+        setFormData((prevData) => ({
+            ...prevData,
+            email: value,
+        }));
+        setVerificationCodeValid(false);
+        setEmailValid(regex.test(value));
+    };
+
+    const handlePassword = (e) => {
+        const value = e.target.value;
+        const regex = new RegExp('^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[$@!%*#?&])[a-zA-Z0-9$@!%*#?&]{8,}$');
+        setFormData((prevData) => ({
+            ...prevData,
+            password: value,
+        }));
+        setInputCheckPassword(regex.test(value));
+    }
+    
     const emailAuth = (e) => {
         // 이메일인증 버튼 누르면 실행
         e.preventDefault();
         if (formData.email === "") {
             alert("이메일을 입력해 주세요.");
-        } else {
+        } else if (!emailValid){
+            alert("이메일 형식을 맞춰주세요.");
+        }
+        else{
             axios({
-                url: `http://localhost:8000/api/users/${formData.email}`,
+                url: `/api/users/${formData.email}`,
                 method: "GET",
             })
                 .then((res) => {
@@ -108,13 +146,18 @@ const Signup = () => {
         }
     };
 
+    const correctStyle = {
+        color: "green",
+        fontWeight: "bold",
+    };
+
     const handleCheckPassword = (e) => {
         setInputCheckPassword(e.target.value);
     };
 
     const handleConfirmPassword = (e) => {
         setConfirmPassword(e.target.value);
-        // setPasswordMatchError(password !== e.target.value);
+        setPasswordMatchError(formData.password !== e.target.value);
     };
 
     const handleSubmit = async (e) => {
@@ -123,15 +166,40 @@ const Signup = () => {
         // if (password !== confirmPassword) {
         //     return; // 비밀번호가 일치하지 않을 경우 아무 작업도 하지 않음
         // }
+        if (!userIdCheck) {
+            alert("아이디 중복체크를 해주세요.");
+            return;
+        }
 
+        if (!userNickNameCheck) {
+            alert("닉네임 중복체크를 해주세요.");
+            return;
+        }
+        if (!emailValid) {
+            alert("이메일 형식 맞게 해주세요.");
+            return;
+        }
+        if (!verificationCodeValid) {
+            alert("이메일 인증을 해주세요.");
+            return;
+        }
+        if (!inputCheckPassword) {
+            alert("올바른 비밀번호를 입력해주세요.");
+            return;
+        }
+        if (passwordMatchError) {
+            
+            alert("비밀번호가 일치하지않습니다.");
+            return;
+        }
+        console.log(formData);
         try {
             const response = await axios.post(
-                "http://localhost:8000/api/users",
+                "/api/users",
                 formData
             );
             // 회원가입이 성공하면 사용자 정보를 스토어에 저장
             console.log(response.data);
-            // dispatch({ type: "USER_LOGIN_SUCCESS", payload: response.data });
             navigate("/login"); // 회원가입 후 리다이렉션할 페이지 설정
         } catch (error) {
             // 회원가입 실패 시 에러 처리
@@ -143,15 +211,23 @@ const Signup = () => {
     };
 
     const handleVerificationCode = (e) => {
-        setFormData((prevState) => ({
-            ...prevState,
-            verificationCode: e.target.value,
-        }));
+        setVerificationCode(e.target.value);
     };
+    
+    const onClickVerficationCode = async (e) => {
+        await axios.post(`/api/users/${formData.email}`, { key: verificationCode })
+            .then((res) => {
+                setVerificationCodeValid(true);
+            })
+            .catch((err) => {
+                alert("인증코드를 다시 입력하세요.");
+                setVerificationCodeValid(false);
+        })
+    }
 
     const handleFullName = (e) => {
-        dispatch(setFullName(e.target.value));
-        dispatch(setNotAllow());
+        // dispatch(setFullName(e.target.value));
+        // dispatch(setNotAllow());
         setFormData((prevState) => ({
             ...prevState,
             fullName: e.target.value,
@@ -172,14 +248,15 @@ const Signup = () => {
 
                     <div className="contentWrap">
                         <S.InputField>
-                            <div className="inputTitle">실명</div>
+                            <div className="inputTitle">이름</div>
                             <div className="inputWrap">
                                 <input
                                     type="text"
+                                    name="username"
                                     className="input"
-                                    placeholder="실명을 입력하세요"
-                                    value={formData.fullName}
-                                    onChange={handleFullName}
+                                    placeholder="이름을 입력하세요"
+                                    value={formData.username}
+                                    onChange={handleChange}
                                 />
                             </div>
 
@@ -199,15 +276,16 @@ const Signup = () => {
                                 <input
                                     type="text"
                                     className="input"
+                                    name="userId"
                                     placeholder="사용하실 ID를 입력하세요"
-                                    // value={id}
-                                    // onChange={handleId}
+                                    value={formData.userId}
+                                    onChange={handleChange}
                                 />
                             </div>
 
                             <S.CheckBtn
                                 type="button"
-                                onClick={() => handleDuplicateCheck("fullName")}
+                                onClick={onClickCheckId}
                                 style={{
                                     position: "absolute",
                                     top: "150px", // 원하는 위치로 조정
@@ -224,20 +302,20 @@ const Signup = () => {
                                     type="text"
                                     className="input"
                                     placeholder="사용하실 email을 입력하세요"
-                                    // value={formData.email}
-                                    // onChange={handleEmail}
+                                    value={formData.email}
+                                    onChange={handleEmail}
                                 />
                             </div>
 
                             <div className="errorMessageWrap">
-                                {/* {!emailValid && email.length > 0 && (
+                                {!emailValid && formData.email.length != 0 && (
                                         <div>올바른 이메일을 입력하세요</div>
-                                    )} */}
+                                    )}
                             </div>
 
                             <S.CheckBtn
                                 type="button"
-                                onClick={() => handleDuplicateCheck("fullName")}
+                                onClick={emailAuth}
                                 style={{
                                     position: "absolute",
                                     top: "230px", // 원하는 위치로 조정
@@ -246,6 +324,7 @@ const Signup = () => {
                             >
                                 인증
                             </S.CheckBtn>
+                            
 
                             <S.VerificationField
                                 style={{ marginLeft: "-15px" }}
@@ -256,15 +335,24 @@ const Signup = () => {
                                         type="text"
                                         className="verificationInput"
                                         placeholder="인증번호 입력"
-                                        value={formData.verificationCode}
+                                        value={verificationCode}
                                         onChange={handleVerificationCode}
                                     />
                                 </div>
                             </S.VerificationField>
 
+                            <div className="errorMessageWrap">
+                                {verificationCodeValid && (
+                                        <div style={correctStyle}>이메일 인증 완료.</div>
+                                )}
+                                {verificationCodeValid !== null && !verificationCodeValid && (
+                                        <div>이메일 인증 실패.</div>
+                                    )}
+                            </div>
+
                             <S.CheckBtn
                                 type="button"
-                                onClick={() => handleDuplicateCheck("fullName")}
+                                onClick={onClickVerficationCode}
                                 style={{
                                     position: "absolute",
                                     top: "299px", // 원하는 위치로 조정
@@ -279,9 +367,10 @@ const Signup = () => {
                                 <input
                                     type="text"
                                     className="input"
+                                    name="nickname"
                                     placeholder="사용하실 닉네임을 입력하세요"
-                                    // value={nickname}
-                                    // onChange={handleNickname}
+                                    value={formData.nickname}
+                                    onChange={handleChange}
                                 />
                             </div>
                             <div className="errorMessageWrap">
@@ -292,7 +381,7 @@ const Signup = () => {
 
                             <S.CheckBtn
                                 type="button"
-                                onClick={() => handleDuplicateCheck("fullName")}
+                                onClick={onClickCheckNickName}
                                 style={{
                                     position: "absolute",
                                     top: "385px", // 원하는 위치로 조정
@@ -307,15 +396,19 @@ const Signup = () => {
                                 <input
                                     type="password"
                                     className="input"
-                                    // value={formData.password}
-                                    // onChange={handlePassword}
-                                    placeholder="영문,숫자,특수기호가 포함되어야 합니다."
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handlePassword}
+                                    placeholder="영문,숫자,특수기호 포함 8글자 이상 되어야 합니다."
                                 />
                             </div>
                             <div className="errorMessageWrap">
-                                {/* {!passwordValid && password.length > 0 && (
-                                        <div>올바른 비밀번호를 입력하세요</div>
-                                    )} */}
+                                {!inputCheckPassword && formData.password.length != 0 && (
+                                        <div>형식에 맞게 입력해 주세요.</div>
+                                )}
+                                {inputCheckPassword && (
+                                        <div style={correctStyle}>사용가능한 비밀번호 입니다.</div>
+                                    )}
                             </div>
                             <div className="inputTitle">비밀번호 확인</div>
                             <div className="inputWrap">
@@ -331,6 +424,9 @@ const Signup = () => {
                                 {passwordMatchError && (
                                     <div>비밀번호가 일치하지 않습니다.</div>
                                 )}
+                                {passwordMatchError !== null && !passwordMatchError &&(
+                                    <div style={correctStyle}>비밀번호가 일치합니다.</div>
+                                )}
                             </div>
                         </S.InputField>
                     </div>
@@ -338,7 +434,7 @@ const Signup = () => {
                     <div>
                         <S.CheckBtn
                             type="button"
-                            onClick={() => handleDuplicateCheck("fullName")}
+                            onClick={handleSubmit}
                             style={{
                                 position: "absolute",
                                 top: "670px", // 원하는 위치로 조정
