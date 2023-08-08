7dae716c7dbc62e46da93a27e33740b6b4762b92
이동현 <dpwk8@naver.com>
2023-08-08 15:37:35 +0900

    feat: id, pw 찾기 페이지 뼈대

---
+++ frontend/src/pages/user/FindPasswordPage.js
@@ -0,0 +1,83 @@
+import { dividerClasses } from "@mui/material";
+import { useCallback, useState, useEffect } from "react";
+import axios from "axios";
+import * as S from "../../components/auth/UserStyledComponents";
+
+const FindPassword = () => {
+    const [inputUserId, setInputUserId] = useState("");
+    const [inputEmail, setInputEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios
+            .get("http://localhost:8000/api/users/find/password", {
+                params: {
+                    userId: inputUserId,
+                    email: inputEmail,
+                },
+            })
+            .then((response) => {
+                console.log(response.data.msg);
+                setMessage(response.data.msg);
+                alert("Status : " + response.data.msg);
+                setError(null);
+            })
+            .catch((error) => {
+                console.log(error);
+                setError("이메일 전송 실패!");
+            });
+    };
+
+    return (
+        <S.loginForm>
+            <form onSubmit={handleSubmit}>
+                <S.page>
+                    <S.wrap>
+                        <h2>개인회원 ID찾기</h2>
+                        <h3>가입시 입력한 이메일을 입력해주세요</h3>
+                        <S.contentWrap>
+                            <S.inputTitle>ID</S.inputTitle>
+                            <S.inputWrap>
+                                <S.inputEmailContent
+                                    type="text"
+                                    className="input"
+                                    placeholder="ID를 입력해주세요"
+                                    value={inputUserId}
+                                    onChange={(e) =>
+                                        setInputUserId(e.target.value)
+                                    }
+                                />
+                            </S.inputWrap>
+                            <S.inputTitle>E-MAIL</S.inputTitle>
+                            <S.inputWrap>
+                                <S.inputEmailContent
+                                    type="email"
+                                    className="input"
+                                    placeholder="E-MAIL을 입력해주세요"
+                                    value={inputEmail}
+                                    onChange={(e) =>
+                                        setInputEmail(e.target.value)
+                                    }
+                                />
+                            </S.inputWrap>
+                        </S.contentWrap>
+                        <div>
+                            <S.submitButton
+                                className="bottomButton"
+                                // onClick={handleSubmit}
+                            >
+                                찾기
+                            </S.submitButton>
+                        </div>
+                    </S.wrap>
+                </S.page>
+            </form>
+            {message && <p>Status : {message}</p>}
+            {error && <p>{error}</p>}
+        </S.loginForm>
+    );
+};
+
+export default FindPassword;
