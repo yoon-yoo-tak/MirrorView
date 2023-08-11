3a26e5a6ec561d10305552214917006b8f879d63
sangil <gsl0505@naver.com>
2023-08-11 10:07:50 +0900

    feat: interview changebutton 수정

--- frontend/src/components/studyroom/StudyRoomIntervieweeComponent.js
+++ frontend/src/components/studyroom/StudyRoomIntervieweeComponent.js
@@ -7,76 +7,92 @@ import { WebSocketContext } from "WebSocketContext";
 import { setNicknames } from "store/InterviewWebSocketStore";
 
 const StudyRoomInterviewee = ({ peopleList, subscribers, streamManager }) => {
-  const { client } = useContext(WebSocketContext);
-  const [selectSubscriber, setSelectSubscriber] = useState(null);
-  const selectPerson = (e) => {
-    setSelectSubscriber(e);
-  };
-  const dispatch = useDispatch();
-  const { currentRoom } = useSelector((state) => state.interviewWebSocket);
-  const { user } = useSelector((state) => state.auth);
-
-  const handleExit = () => {
-    const exitData = {
-      type: "ROOM_START_CANCEL",
+    const { client } = useContext(WebSocketContext);
+    const [selectSubscriber, setSelectSubscriber] = useState(null);
+    const selectPerson = (e) => {
+        setSelectSubscriber(e);
     };
-    client.send(
-      `/app/interviewrooms/${currentRoom.id}`,
-      {},
-      JSON.stringify(exitData)
-    );
-    console.log("나가기 동작");
-  };
+    const dispatch = useDispatch();
+    const { currentRoom } = useSelector((state) => state.interviewWebSocket);
+    const { user } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    dispatch(
-      setNicknames(
-        currentRoom.members
-          .filter((member) => member.nickname !== user.nickname)
-          .map((member) => member.nickname)
-      )
-    );
-  }, []);
-  return (
-    <S.page>
-      <S.vieweeWrap>
-        <S.mainWrap>
-          <S.mainBox>
-            {selectSubscriber && (
-              <>
-                {JSON.parse(selectSubscriber.stream.connection.data).clientData}
-                <SubscriberVideo
-                  subscriber={selectSubscriber}></SubscriberVideo>
-              </>
-            )}
-          </S.mainBox>
-          <S.roomTitle>면접방 제목</S.roomTitle>
-          <S.exitRoom menu="viewee" onClick={handleExit}>
-            나가기
-          </S.exitRoom>
-        </S.mainWrap>
+    const handleExit = () => {
+        const exitData = {
+            type: "ROOM_START_CANCEL",
+        };
+        client.send(
+            `/app/interviewrooms/${currentRoom.id}`,
+            {},
+            JSON.stringify(exitData)
+        );
+        console.log("나가기 동작");
+    };
 
-        <S.leftBox>
-          <S.boxes onClick={() => selectPerson(streamManager)}>
-            {JSON.parse(streamManager.stream.connection.data).clientData}
-            <SubscriberVideo
-              subscriber={streamManager}
-              key={
-                streamManager.stream.connection.connectionId
-              }></SubscriberVideo>
-          </S.boxes>
-          {subscribers.map((sub) => (
-            <S.boxes onClick={() => selectPerson(sub)}>
-              {JSON.parse(sub.stream.connection.data).clientData}
-              <SubscriberVideo
-                subscriber={sub}
-                key={sub.stream.connection.connectionId}></SubscriberVideo>
-            </S.boxes>
-          ))}
-        </S.leftBox>
-      </S.vieweeWrap>
-    </S.page>
-  );
+    useEffect(() => {
+        dispatch(
+            setNicknames(
+                currentRoom.members
+                    .filter((member) => member.nickname !== user.nickname)
+                    .map((member) => member.nickname)
+            )
+        );
+    }, []);
+    return (
+        <S.page>
+            <S.vieweeWrap>
+                <S.mainWrap>
+                    <S.mainBox>
+                        {selectSubscriber && (
+                            <>
+                                {
+                                    JSON.parse(
+                                        selectSubscriber.stream.connection.data
+                                    ).clientData
+                                }
+                                <SubscriberVideo
+                                    subscriber={selectSubscriber}
+                                ></SubscriberVideo>
+                            </>
+                        )}
+                    </S.mainBox>
+                    <S.roomTitle>면접방 제목</S.roomTitle>
+                    <S.exitRoom menu="viewee" onClick={handleExit}>
+                        나가기
+                    </S.exitRoom>
+                </S.mainWrap>
+
+                <S.leftBox>
+                    <S.boxes onClick={() => selectPerson(streamManager)}>
+                        {streamManager && (
+                            <>
+                                {
+                                    JSON.parse(
+                                        streamManager.stream.connection.data
+                                    ).clientData
+                                }
+                                <SubscriberVideo
+                                    subscriber={streamManager}
+                                    key={
+                                        streamManager.stream.connection
+                                            .connectionId
+                                    }
+                                ></SubscriberVideo>
+                            </>
+                        )}
+                    </S.boxes>
+                    {subscribers.map((sub) => (
+                        <S.boxes onClick={() => selectPerson(sub)}>
+                            {JSON.parse(sub.stream.connection.data).clientData}
+                            <SubscriberVideo
+                                subscriber={sub}
+                                key={sub.stream.connection.connectionId}
+                            ></SubscriberVideo>
+                        </S.boxes>
+                    ))}
+                </S.leftBox>
+            </S.vieweeWrap>
+        </S.page>
+    );
 };
 
 export default StudyRoomInterviewee;
