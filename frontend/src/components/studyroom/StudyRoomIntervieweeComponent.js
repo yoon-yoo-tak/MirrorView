f0e269c583917731c6344ae8adf13a45d1375c77
sangil <gsl0505@naver.com>
2023-08-12 18:10:44 +0900

    feat: 인터뷰 오디오 비디오 설정 버튼 기능 추가

--- frontend/src/components/studyroom/StudyRoomIntervieweeComponent.js
+++ frontend/src/components/studyroom/StudyRoomIntervieweeComponent.js
@@ -7,138 +7,133 @@ import { WebSocketContext } from "WebSocketContext";
 import { setNicknames } from "store/InterviewWebSocketStore";
 
 const StudyRoomInterviewee = (props) => {
-    const {
-        peopleList,
-        streamManager,
-        subscribers,
-        setIsVideoOn,
-        isVideoOn,
-        setIsAudioOn,
-        isAudioOn,
-    } = props;
-    const { client } = useContext(WebSocketContext);
-    const [selectSubscriber, setSelectSubscriber] = useState(null);
-    const selectPerson = (e) => {
-        setSelectSubscriber(e);
+  const {
+    peopleList,
+    streamManager,
+    subscribers,
+    setIsVideoOn,
+    isVideoOn,
+    setIsAudioOn,
+    isAudioOn,
+  } = props;
+  const { client } = useContext(WebSocketContext);
+  const [selectSubscriber, setSelectSubscriber] = useState(null);
+  const selectPerson = (e) => {
+    setSelectSubscriber(e);
+  };
+  const dispatch = useDispatch();
+  const { currentRoom } = useSelector((state) => state.interviewWebSocket);
+  const { user } = useSelector((state) => state.auth);
+
+  const handleExit = () => {
+    const exitData = {
+      type: "ROOM_START_CANCEL",
     };
-    const dispatch = useDispatch();
-    const { currentRoom } = useSelector((state) => state.interviewWebSocket);
-    const { user } = useSelector((state) => state.auth);
-
-    const handleExit = () => {
-        const exitData = {
-            type: "ROOM_START_CANCEL",
-        };
-        client.send(
-            `/app/interviewrooms/${currentRoom.id}`,
-            {},
-            JSON.stringify(exitData)
-        );
-        console.log("나가기 동작");
-    };
-
-    useEffect(() => {
-        dispatch(
-            setNicknames(
-                currentRoom.members
-                    .filter((member) => member.nickname !== user.nickname)
-                    .map((member) => member.nickname)
-            )
-        );
-    }, []);
-
-    const handleCamEnable = () => {
-        setIsVideoOn(!isVideoOn);
-    };
-
-    const handleMicEnable = () => {
-        setIsAudioOn(!isAudioOn);
-    };
-
-    return (
-        <S.page>
-            <S.vieweeWrap>
-                <S.mainWrap>
-                    <div>
-                        <S.mainBox>
-                            {selectSubscriber && (
-                                <>
-                                    {
-                                        JSON.parse(
-                                            selectSubscriber.stream.connection
-                                                .data
-                                        ).clientData
-                                    }
-                                    <SubscriberVideo
-                                        subscriber={selectSubscriber}
-                                    ></SubscriberVideo>
-                                </>
-                            )}
-                        </S.mainBox>
-                        <S.roomTitle>면접방 제목</S.roomTitle>
-                    </div>
-                    <S.exitRoom menu="viewee" onClick={handleExit}>
-                        나가기
-                    </S.exitRoom>
-                </S.mainWrap>
-
-                <S.leftBox>
-                    <S.boxes onClick={() => selectPerson(streamManager)}>
-                        {streamManager && (
-                            <>
-                                {
-                                    JSON.parse(
-                                        streamManager.stream.connection.data
-                                    ).clientData
-                                }
-                                <S.videoParent>
-                                    <SubscriberVideo
-                                        subscriber={streamManager}
-                                        key={
-                                            streamManager.stream.connection
-                                                .connectionId
-                                        }
-                                    ></SubscriberVideo>
-                                    {/* <S.StyledVideo /> */}
-                                    {isVideoOn && (
-                                        <S.videoControlon
-                                            onClick={handleCamEnable}
-                                            value="viewer"
-                                        />
-                                    )}
-                                    {!isVideoOn && (
-                                        <S.videoControloff
-                                            onClick={handleCamEnable}
-                                            value="viewer"
-                                        />
-                                    )}
-                                    {isAudioOn && (
-                                        <S.micControlon
-                                            onClick={handleMicEnable}
-                                        />
-                                    )}
-                                    {!isAudioOn && (
-                                        <S.micControloff
-                                            onClick={handleMicEnable}
-                                        />
-                                    )}
-                                </S.videoParent>
-                            </>
-                        )}
-                    </S.boxes>
-                    {subscribers.map((sub) => (
-                        <S.boxes onClick={() => selectPerson(sub)}>
-                            {JSON.parse(sub.stream.connection.data).clientData}
-                            <SubscriberVideo
-                                subscriber={sub}
-                                key={sub.stream.connection.connectionId}
-                            ></SubscriberVideo>
-                        </S.boxes>
-                    ))}
-                </S.leftBox>
-            </S.vieweeWrap>
-        </S.page>
+    client.send(
+      `/app/interviewrooms/${currentRoom.id}`,
+      {},
+      JSON.stringify(exitData)
     );
+    console.log("나가기 동작");
+  };
+
+  useEffect(() => {
+    dispatch(
+      setNicknames(
+        currentRoom.members
+          .filter((member) => member.nickname !== user.nickname)
+          .map((member) => member.nickname)
+      )
+    );
+  }, []);
+
+  const handleCamEnable = () => {
+    if (isVideoOn) {
+      streamManager.publishVideo(false);
+      setIsVideoOn(false);
+    } else {
+      streamManager.publishVideo(true);
+      setIsVideoOn(true);
+    }
+  };
+
+  const handleMicEnable = () => {
+    if (isAudioOn) {
+      streamManager.publishAudio(false);
+      setIsAudioOn(false);
+    } else {
+      streamManager.publishAudio(true);
+      setIsAudioOn(true);
+    }
+  };
+
+  return (
+    <S.page>
+      <S.vieweeWrap>
+        <S.mainWrap>
+          <div>
+            <S.mainBox>
+              {selectSubscriber && (
+                <>
+                  {
+                    JSON.parse(selectSubscriber.stream.connection.data)
+                      .clientData
+                  }
+                  <SubscriberVideo
+                    subscriber={selectSubscriber}
+                  ></SubscriberVideo>
+                </>
+              )}
+            </S.mainBox>
+            <S.roomTitle>면접방 제목</S.roomTitle>
+          </div>
+          <S.exitRoom menu="viewee" onClick={handleExit}>
+            나가기
+          </S.exitRoom>
+        </S.mainWrap>
+
+        <S.leftBox>
+          <S.boxes onClick={() => selectPerson(streamManager)}>
+            {streamManager && (
+              <>
+                {JSON.parse(streamManager.stream.connection.data).clientData}
+                <S.videoParent>
+                  <SubscriberVideo
+                    subscriber={streamManager}
+                    key={streamManager.stream.connection.connectionId}
+                  ></SubscriberVideo>
+                  {/* <S.StyledVideo /> */}
+                  {isVideoOn && (
+                    <S.videoControlon
+                      onClick={handleCamEnable}
+                      value="viewer"
+                    />
+                  )}
+                  {!isVideoOn && (
+                    <S.videoControloff
+                      onClick={handleCamEnable}
+                      value="viewer"
+                    />
+                  )}
+                  {isAudioOn && <S.micControlon onClick={handleMicEnable} />}
+                  {!isAudioOn && <S.micControloff onClick={handleMicEnable} />}
+                </S.videoParent>
+              </>
+            )}
+          </S.boxes>
+          {subscribers.map((sub) => (
+            <S.boxes onClick={() => selectPerson(sub)}>
+              {JSON.parse(sub.stream.connection.data).clientData}
+              <SubscriberVideo
+                subscriber={sub}
+                key={sub.stream.connection.connectionId}
+              ></SubscriberVideo>
+            </S.boxes>
+          ))}
+        </S.leftBox>
+      </S.vieweeWrap>
+    </S.page>
+  );
 };
 
 export default StudyRoomInterviewee;
--- frontend/src/components/studyroom/StudyRoomInterviewerComponent.js
+++ frontend/src/components/studyroom/StudyRoomInterviewerComponent.js
@@ -7,132 +7,126 @@ import SubscriberVideo from "./studyroominterviewer/SubscriberVideo";
 import { useEffect, useState } from "react";
 import { setNicknames } from "store/InterviewWebSocketStore";
 const StudyRoomInterviewer = (props) => {
-    const {
-        peopleList,
-        streamManager,
-        subscribers,
-        questionList,
-        setQuestionList,
-        // feedbackList,
-        setFeedbackList,
+  const {
+    peopleList,
+    streamManager,
+    subscribers,
+    questionList,
+    setQuestionList,
+    // feedbackList,
+    setFeedbackList,
 
-        setIsVideoOn,
-        isVideoOn,
-        setIsAudioOn,
-        isAudioOn,
-    } = props;
+    setIsVideoOn,
+    isVideoOn,
+    setIsAudioOn,
+    isAudioOn,
+  } = props;
 
-    const gosim = useSelector((state) => state.interview.feedbackList[0]);
-    const { currentRoom } = useSelector((state) => state.interviewWebSocket);
-    const { user } = useSelector((state) => state.auth);
-    const [selectSubscriber, setSelectSubscriber] = useState(null);
-    const dispatch = useDispatch();
-    const test = (e) => {
-        setSelectSubscriber(e);
-    };
-    useEffect(() => {
-        dispatch(
-            setNicknames(
-                currentRoom.members
-                    .filter((member) => member.nickname !== user.nickname)
-                    .map((member) => member.nickname)
-            )
-        );
-    }, []);
-
-    const handleCamEnable = () => {
-        setIsVideoOn(!isVideoOn);
-    };
-
-    const handleMicEnable = () => {
-        setIsAudioOn(!isAudioOn);
-    };
-
-    return (
-        <S.page>
-            <S.interviewerWrap>
-                <S.videoSection>
-                    {/* <S.videoWrap> */}
-                    <S.mainContainer>
-                        <S.mainVideo>
-                            <SubscriberVideo
-                                subscriber={selectSubscriber}
-                            ></SubscriberVideo>
-                        </S.mainVideo>
-                    </S.mainContainer>
-                    <S.lastVideos>
-                        <S.lastVideoEach onClick={() => test(streamManager)}>
-                            {streamManager && (
-                                <>
-                                    {
-                                        JSON.parse(
-                                            streamManager.stream.connection.data
-                                        ).clientData
-                                    }
-                                    님의 화면입니다
-                                    <S.videoParent>
-                                        <SubscriberVideo
-                                            subscriber={streamManager}
-                                            key={
-                                                streamManager.stream.connection
-                                                    .connectionId
-                                            }
-                                        ></SubscriberVideo>
-                                        {/* <S.StyledVideo /> */}
-                                        {isVideoOn && (
-                                            <S.videoControlon
-                                                onClick={handleCamEnable}
-                                                value="viewer"
-                                            />
-                                        )}
-                                        {!isVideoOn && (
-                                            <S.videoControloff
-                                                onClick={handleCamEnable}
-                                                value="viewer"
-                                            />
-                                        )}
-                                        {isAudioOn && (
-                                            <S.micControlon
-                                                onClick={handleMicEnable}
-                                            />
-                                        )}
-                                        {!isAudioOn && (
-                                            <S.micControloff
-                                                onClick={handleMicEnable}
-                                            />
-                                        )}
-                                    </S.videoParent>
-                                </>
-                            )}
-                        </S.lastVideoEach>
-                        {subscribers.map((sub) => (
-                            <S.lastVideoEach onClick={() => test(sub)}>
-                                {
-                                    JSON.parse(sub.stream.connection.data)
-                                        .clientData
-                                }
-                                님의 화면입니다
-                                <SubscriberVideo
-                                    subscriber={sub}
-                                    key={sub.stream.connection.connectionId}
-                                ></SubscriberVideo>
-                            </S.lastVideoEach>
-                        ))}
-                    </S.lastVideos>
-                    {/* </S.videoWrap> */}
-                </S.videoSection>
-                <S.secondSection>
-                    <InterviewerSection
-                        peopleList={peopleList}
-                        // questionList={questionList}
-                        // setQuestionList={setQuestionList}
-                        // feedbackList={feedbackList}
-                        // setFeedbackList={setFeedbackList}
-                    />
-                </S.secondSection>
-            </S.interviewerWrap>
-        </S.page>
+  const gosim = useSelector((state) => state.interview.feedbackList[0]);
+  const { currentRoom } = useSelector((state) => state.interviewWebSocket);
+  const { user } = useSelector((state) => state.auth);
+  const [selectSubscriber, setSelectSubscriber] = useState(null);
+  const dispatch = useDispatch();
+  const test = (e) => {
+    setSelectSubscriber(e);
+  };
+  useEffect(() => {
+    dispatch(
+      setNicknames(
+        currentRoom.members
+          .filter((member) => member.nickname !== user.nickname)
+          .map((member) => member.nickname)
+      )
     );
+  }, []);
+
+  const handleCamEnable = () => {
+    if (isVideoOn) {
+      streamManager.publishVideo(false);
+      setIsVideoOn(false);
+    } else {
+      streamManager.publishVideo(true);
+      setIsVideoOn(true);
+    }
+  };
+
+  const handleMicEnable = () => {
+    if (isAudioOn) {
+      streamManager.publishAudio(false);
+      setIsAudioOn(false);
+    } else {
+      streamManager.publishAudio(true);
+      setIsAudioOn(true);
+    }
+  };
+
+  return (
+    <S.page>
+      <S.interviewerWrap>
+        <S.videoSection>
+          {/* <S.videoWrap> */}
+          <S.mainContainer>
+            <S.mainVideo>
+              <SubscriberVideo subscriber={selectSubscriber}></SubscriberVideo>
+            </S.mainVideo>
+          </S.mainContainer>
+          <S.lastVideos>
+            <S.lastVideoEach onClick={() => test(streamManager)}>
+              {streamManager && (
+                <>
+                  {JSON.parse(streamManager.stream.connection.data).clientData}
+                  님의 화면입니다
+                  <S.videoParent>
+                    <SubscriberVideo
+                      subscriber={streamManager}
+                      key={streamManager.stream.connection.connectionId}
+                    ></SubscriberVideo>
+                    {/* <S.StyledVideo /> */}
+                    {isVideoOn && (
+                      <S.videoControlon
+                        onClick={handleCamEnable}
+                        value="viewer"
+                      />
+                    )}
+                    {!isVideoOn && (
+                      <S.videoControloff
+                        onClick={handleCamEnable}
+                        value="viewer"
+                      />
+                    )}
+                    {isAudioOn && <S.micControlon onClick={handleMicEnable} />}
+                    {!isAudioOn && (
+                      <S.micControloff onClick={handleMicEnable} />
+                    )}
+                  </S.videoParent>
+                </>
+              )}
+            </S.lastVideoEach>
+            {subscribers.map((sub) => (
+              <S.lastVideoEach onClick={() => test(sub)}>
+                {JSON.parse(sub.stream.connection.data).clientData}
+                님의 화면입니다
+                <SubscriberVideo
+                  subscriber={sub}
+                  key={sub.stream.connection.connectionId}
+                ></SubscriberVideo>
+              </S.lastVideoEach>
+            ))}
+          </S.lastVideos>
+          {/* </S.videoWrap> */}
+        </S.videoSection>
+        <S.secondSection>
+          <InterviewerSection
+            peopleList={peopleList}
+            // questionList={questionList}
+            // setQuestionList={setQuestionList}
+            // feedbackList={feedbackList}
+            // setFeedbackList={setFeedbackList}
+          />
+        </S.secondSection>
+      </S.interviewerWrap>
+    </S.page>
+  );
 };
 
 export default StudyRoomInterviewer;
